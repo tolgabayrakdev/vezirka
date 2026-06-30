53659e3619c9934d98426e03b2aac171bb555107
Tolga BAYRAK <bayraktolga28@gmail.com>
2026-06-30 08:22:53 +0300

    feat: new web project

---
+++ web/src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+import Home from "./views/Home";
+
+const router = createBrowserRouter([
+  {
+    element: <Home />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
